758d7a2aede8ffd81079f8c753a358ae8386c3e6
longf <longf@DESKTOP-2GJ1G8J>
2022-09-12 17:35:41 +0200

    Add project files.

---
+++ Uppgift 2.11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uppgift2_11
+{
+    class Program
+    {
+        static void Main(string[]Args)
+        {
+            Console.WriteLine("Hur långt hoppade du Elin?");
+            string x = Console.ReadLine();
+            Console.WriteLine("Hur långt hoppade du Alma?");
+            string y = Console.ReadLine();
+            int a = int.Parse(x);
+            int b = int.Parse(y);
+            int c = a - b;
+            Console.Write(" Elin hoppade " + c);
+            Console.Write(" meter längre än Alma.");
+
+
+        }
+    }
+}
